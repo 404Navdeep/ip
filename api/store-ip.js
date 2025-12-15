0ecfa7a2f90785728d642bad23401f2203952d75
Mr-Singh-69 <navdeep13dps@gmail.com>
2025-12-15 17:47:56 +0530

    add cors headers

--- api/store-ip.js
+++ api/store-ip.js
@@ -4,6 +4,11 @@ let client;
 let db;
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).end();
 
   try {
